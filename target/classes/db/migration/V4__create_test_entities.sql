a5c63121c9021b345f816fca9c26a58bb67632e7
HunsonAbadeer1989 <48861096+HunsonAbadeer1989@users.noreply.github.com>
2021-01-28 17:38:42 +0300

    comment controller

---
+++ target/classes/db/migration/V4__create_test_entities.sql
@@ -0,0 +1,27 @@
+insert into users (is_moderator, reg_time, name, email, password)
+    values ( 0, NOW(), 'ivan', 'ivanemail@yandex.ru', "$2y$12$9kYbkGnPHt.Ro0W.DuFbT.Hq2/Jp.8ZYj0ji/41w8828RVqvo5.Ni"),
+    ( 1, NOW(), 'eugene', 'eugeneemail@yandex.ru', "$2y$12$9kYbkGnPHt.Ro0W.DuFbT.Hq2/Jp.8ZYj0ji/41w8828RVqvo5.Ni");
+
+insert into posts (is_active, moderation_status, moderator_id, user_id, publication_time, title, text, view_count)
+    values ( 1, 'ACCEPTED', (SELECT id FROM users WHERE is_moderator like 1),
+            (SELECT id FROM users WHERE is_moderator like 0), ADDDATE(NOW(), INTERVAL -780 DAY), 'MyFirstPost', 'Test post text', 1),
+            ( 0, 'ACCEPTED', (SELECT id FROM users WHERE is_moderator like 1),
+                (SELECT id FROM users WHERE name like 'ivan'), ADDDATE(NOW(), INTERVAL -360 DAY), 'MySecondPost', 'Test post text', 1),
+            ( 1, 'ACCEPTED', (SELECT id FROM users WHERE is_moderator like 1),
+                (SELECT id FROM users WHERE name like 'eugene'), ADDDATE(NOW(), INTERVAL -200 DAY), 'SimplePost', 'Simple post text', 2),
+            ( 1, 'ACCEPTED', (SELECT id FROM users WHERE is_moderator like 1),
+                (SELECT id FROM users WHERE name like 'eugene'), ADDDATE(NOW(), INTERVAL -100 DAY), 'ThirdPost', 'Third post text', 3);
+
+insert into post_comments (post_id, user_id, time, text, parent_id)
+    values ( (SELECT id FROM posts WHERE title like 'MyFirstPost'),
+             (SELECT id FROM users WHERE name like 'ivan'),
+              NOW(), 'You totally wright!', 3),
+           ( (SELECT id FROM posts WHERE title like 'MySecondPost'),
+             (SELECT id FROM users WHERE name like 'ivan'),
+              NOW(), 'You totally wrong!', 4);
+
+insert into tags (name) values ('lookAtMe'), ('Java'), ('skillbox');
+
+insert into tag2post (post_id, tag_id) values (1, 1), (2, 3), (4, 2);
+
+
